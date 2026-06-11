e2441d70d89471bdef38e7b248ea7d9d07917d62
ricoroyadm <ricoroy.adm@gmail.com>
2026-06-11 17:27:33 +0700

    index.html + js
    tambah link ke halaman uts

--- js/jadwal.js
+++ js/jadwal.js
@@ -9,12 +9,13 @@
 // 1. Kumpulan URL dalam satu variabel objek
 const links = {
   jadM: "https://drive.google.com/file/d/1JmYcNKcHHwfaemhGpWo9OHekSdZzOFCT/view?usp=drive_link",
+  jadUTS: "/ujian",
 };
 
 // 2. Ambil semua elemen button
-const btnJadM = document.getElementById("btnJadM");
+const btnJadUTS = document.getElementById("btnJadUTS");
 
 // 3. Pasang fungsi klik untuk masing-masing tombol
-btnJadM.addEventListener("click", () => {
-  window.open(links.jadM, "_blank");
+btnJadUTS.addEventListener("click", () => {
+  window.open(links.jadUTS, "_blank");
 });
